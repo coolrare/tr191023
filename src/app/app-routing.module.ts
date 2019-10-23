873b8335a9e60b50186e69c5639d95e8f64b50fc
Will <doggy.huang@gmail.com>
2019-10-23 10:41:21 +0800

    完成多頁面路由開發 (dashboard & cards)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { CardsComponent } from './cards/cards.component';
 
 
 const routes: Routes = [
-
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'cards', component: CardsComponent }
 ];
 
 @NgModule({
