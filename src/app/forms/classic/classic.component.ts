4b84b4b307115c04d4c98e29366cd5e5aa1bb39c
Will <doggy.huang@gmail.com>
2019-10-24 11:57:26 +0800

    示範 ReactiveForms 基本用法

--- src/app/forms/classic/classic.component.ts
+++ src/app/forms/classic/classic.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, FormArray } from '@angular/forms';
 
 @Component({
   selector: 'app-classic',
@@ -7,9 +8,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ClassicComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup;
+
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
+
+    this.form = this.fb.group({
+      name: 'Will',
+      email: 'user@example.com',
+      tel: this.fb.control('0944-444444'),
+      details: this.fb.array([
+      ])
+    });
+
+  }
+
+  fillDetails() {
+    var details = this.form.get('details') as FormArray;
+
+    details.push(this.fb.group({
+
+    }));
   }
 
 }
--- src/app/forms/forms.module.ts
+++ src/app/forms/forms.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 
 import { FormsRoutingModule } from './forms-routing.module';
 import { ClassicComponent } from './classic/classic.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
@@ -11,7 +12,8 @@ import { ClassicComponent } from './classic/classic.component';
   ],
   imports: [
     CommonModule,
-    FormsRoutingModule
+    FormsRoutingModule,
+    ReactiveFormsModule
   ]
 })
 export class FormsModule { }
