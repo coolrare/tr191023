384027db0887b732aebbe3c407a421b44a5dc28f
Will <doggy.huang@gmail.com>
2019-10-24 10:49:29 +0800

    示範 enable() 與 disable() 方法

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,12 +1,12 @@
-import { Component, OnInit } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core';
+import { NgForm, NgModel } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent implements OnInit, AfterViewInit {
 
   login: any = {};
 
@@ -15,11 +15,22 @@ export class LoginComponent implements OnInit {
   ngOnInit() {
   }
 
+  ngAfterViewInit(): void {
+  }
+
   onSubmit(form: NgForm) {
     if (form.valid) {
       // TODO: Send AJAX
       console.log(form.value);
     }
+  }
+
+  setDisabled(ctl: NgModel) {
+    ctl.control.disable();
+  }
+
+  setEnabled(ctl: NgModel) {
+    ctl.control.enable();
   }
 
 }
